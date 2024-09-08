990da97a3454bded2bc1be0c8a8c3b261f70c186
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-09-09 01:40:22 +0330

    git init

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div>hello</div>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <TheHeader />
+        this is index page
+    </div>
+</template>

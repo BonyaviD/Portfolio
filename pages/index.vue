1f31b9bdf1314d2183a824f6529da7134af588af
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-09-26 12:42:49 +0330

    created index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
+<script setup>
+import TheHero from '~/components/sections/TheHero.vue';
+import TheAboutMe from '~/components/sections/TheAboutMe.vue';
+import TheSkills from '~/components/sections/TheSkills.vue';
+import TheExperience from '~/components/sections/TheExperience.vue';
+import TheHobbies from '~/components/sections/TheHobbies.vue';
+</script>
+
 <template>
-    <div>
-        <TheHeader />
-        this is index page
-    </div>
+<div>
+  <TheHero />
+  <TheAboutMe />
+  <TheSkills />
+  <TheExperience />
+  <TheHobbies />
+</div>
+
 </template>
+
+<style scoped>
+</style>

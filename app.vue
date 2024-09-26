595914e46c0e51842905e49dda4c924703c62368
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-09-26 12:41:38 +0330

    add footer to app

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
+<script setup>
+import TheHeader from './components/sections/TheHeader.vue';
+import TheFooter from './components/sections/TheFooter.vue';
+</script>
+
 <template>
   <div id="app">
     <TheHeader />
     <NuxtPage />
+    <TheFooter />
   </div>
 </template>
 <style scoped>

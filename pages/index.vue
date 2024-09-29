24b6924a391140f4f517373971b00c6c1c507af2
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-09-29 10:07:49 +0330

    fix home title name

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <script setup>
-import TheHero from '~/components/sections/TheHero.vue';
-import TheAboutMe from '~/components/sections/TheAboutMe.vue';
+import TheHero from '~/components/sections/TheHeroSection/TheHero.vue';
+import TheAboutMe from '~/components/sections/TheAboutMeSection/TheAboutMe.vue';
 import TheSkills from '~/components/sections/TheSkills.vue';
 import TheExperience from '~/components/sections/TheExperience.vue';
 import TheHobbies from '~/components/sections/TheHobbies.vue';
+
+useHead({
+  titleTemplate: '%s - Home',
+  meta: [
+    { hid: 'description', name: 'description', content: 'پورتفولیو نوید بنیادی' },
+  ],
+})
 </script>
 
 <template>

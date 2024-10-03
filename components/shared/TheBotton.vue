38c57dcf78f2c338178ea79a6823dd0b24a47a04
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-10-04 02:17:15 +0330

    created botton component

---
+++ components/shared/TheBotton.vue
@@ -0,0 +1,104 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount, computed } from "vue";
+import { defineProps } from "vue";
+
+// Define props
+const props = defineProps({
+  to: {
+    type: String,
+  },
+  external: {
+    type: Boolean,
+    default: false
+  },
+  btnSize: {
+    type: [String, Number],
+  },
+  text: {
+    type: String,
+  },
+  Logo: {
+    type: String,
+  },
+  size: {
+    type: Number,
+    default: 25,
+  },
+  opacityMode: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// Compute styles dynamically based on the size prop
+const logoStyle = computed(() => ({
+  width: `${props.size}px`,
+  height: `${props.size}px`,
+}));
+
+const btnSize = computed(() => ({
+  width: `${props.btnSize}px`,
+}));
+
+const btnMode = computed(() => ({
+  backgroundColor: `${
+    props.opacityMode ? "var(--primary-opacity-color)" : "var(--primary-color)"
+  }`,
+  border: `${props.opacityMode ? "2px solid var(--primary-color)" : "none"}`,
+}));
+
+const responsive = ref();
+
+function checkResolution() {
+  if (window.innerWidth <= 560) {
+    responsive.value = true;
+  } else {
+    responsive.value = false;
+  }
+}
+
+onMounted(() => {
+  checkResolution();
+  window.addEventListener('resize', checkResolution);
+});
+
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', checkResolution);
+});
+</script>
+
+<template>
+  <NuxtLink
+    :to="to"
+    target="_blank"
+    :external="external"
+    class="custom-link"
+    :style="[btnMode, btnSize]"
+  >
+    <div class="link-logo" v-if="Logo">
+      <img :src="Logo" :style="logoStyle" alt="Logo" />
+    </div>
+    <div class="link-text" v-if="text && !responsive">{{ text }}</div>
+  </NuxtLink>
+</template>
+
+<style scoped>
+.custom-link {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0 6px;
+  padding: 0.4rem 0.6rem;
+  border-radius: 8px;
+}
+.custom-link .link-text {
+  color: var(--light-text-color);
+}
+
+.custom-link .link-logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>

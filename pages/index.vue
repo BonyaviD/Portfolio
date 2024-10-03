4895521ebdbca88c3957b46d64ae8bdd23d6c3d5
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-10-04 02:15:55 +0330

    update import address

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <script setup>
 import TheHero from '~/components/sections/TheHeroSection/TheHero.vue';
-import TheAboutMe from '~/components/sections/TheAboutMeSection/TheAboutMe.vue';
-import TheSkills from '~/components/sections/TheSkills.vue';
+import TheAboutMe from '~/components/sections/TheAboutMe.vue';
+import TheSkills from '~/components/sections/TheSkills/TheSkills.vue';
 import TheExperience from '~/components/sections/TheExperience.vue';
-import TheHobbies from '~/components/sections/TheHobbies.vue';
+import TheHobbies from '~/components/sections/TheHobbies/TheHobbies.vue';
 
 useHead({
   titleTemplate: '%s - Home',

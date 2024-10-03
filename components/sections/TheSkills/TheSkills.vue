c3b59f5f5fa007ea0ec61018016a1ac2b73d99fa
Navid Bonyadi <navidprinceofpersia@gmail.com>
2024-10-04 02:18:46 +0330

    created skills section

---
+++ components/sections/TheSkills/TheSkills.vue
@@ -0,0 +1,46 @@
+<script setup>
+import SectionTitle from "@/components/shared/TheSectionTitle.vue";
+import TheSkillsItem from "@/components/sections/TheSkills/TheSkillsItem.vue";
+</script>
+<template>
+  <div class="container">
+    <SectionTitle text="Skills" marginTop="100px" marginBottom="50px" />
+    <div class="skills-content">
+      <div class="first-skills skills">
+        <TheSkillsItem text="Html" level="Expert"/>
+        <TheSkillsItem text="Css" level="Expert"/>
+        <TheSkillsItem text="Sass" level="Advanced"/>
+        <TheSkillsItem text="Scss" level="Advanced"/>
+        <TheSkillsItem text="Tailwind CSS" level="Intermediate" />
+        <TheSkillsItem text="Bootstrap" level="Intermediate" /> 
+        <TheSkillsItem text="JavaScript" level="Advanced" />
+        <TheSkillsItem text="TypeScript" level="Beginner " /> 
+        <TheSkillsItem text="HTTP Requests" level="Advanced" />
+        <TheSkillsItem text="RESTful APIs" level="Advanced" />
+        <TheSkillsItem text="Pinia" level="Advanced" />
+        <TheSkillsItem text="Vite" level="Intermediate" />
+        <TheSkillsItem text="GIT" level="Advanced" /> 
+        <TheSkillsItem text="GITHUB" level="Advanced" /> 
+        <TheSkillsItem text="Vue" level="Advanced" /> 
+        <TheSkillsItem text="Nuxt" level="Advanced" /> 
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.skills-content {
+  width: 100%;
+  border-radius: 8px;
+}
+.skills {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  overflow: hidden;
+}
+</style>
---
+++ components/sections/TheSkills/TheSkillsItem.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    text: {
+        type: String
+    },
+    level: {
+        type: String
+    }
+})
+</script>
+
+<template>
+ <div class="skills-name">
+       <div> {{ props.text }}</div>
+        <span class="skill-level">{{ level }}</span>
+    </div>
+   
+</template>
+
+<style scoped>
+.skills-name {
+    /* height: 60px; */
+    padding: 10px 20px;
+    background-color: var(--second-dark-color);
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    gap: 0px 10px;
+}
+
+.skill-level {
+    border: 1px solid var(--primary-color);
+    background-color: var(--primary-opacity-color);
+    padding: 0.2rem 0.6rem;
+    border-radius: 8px;
+    margin-left: 4px;
+}
+</style>
